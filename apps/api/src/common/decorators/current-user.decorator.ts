2ca9d56b8bb52742f0c594b151235b325b1be4f3
ELJORGEVALLEJOS <jorgevallejos934@gmail.com>
2026-02-18 21:04:02 -0300

    refactor: flatten repository to frontend-only structure

--- apps/api/src/common/decorators/current-user.decorator.ts
+++
@@ -1,9 +0,0 @@
-import { createParamDecorator, ExecutionContext } from '@nestjs/common';
-import { JwtUser } from '../auth/jwt-user.interface';
-
-export const CurrentUser = createParamDecorator(
-  (_data: unknown, ctx: ExecutionContext): JwtUser => {
-    const request = ctx.switchToHttp().getRequest<{ user: JwtUser }>();
-    return request.user;
-  },
-);
